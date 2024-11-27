8d41a9d2c0955446e84930a2de2645547bec48a9
Marques <marquesviniciusmelomartins@gmail.com>
2024-11-27 20:22:46 -0300

    Corrigindo arquivos ignorados no repositório

--- src/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log("JWT_SECRET:", process.env.JWT_SECRET);
-});
